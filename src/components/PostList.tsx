e3999d8590c7bd52e6cd39181aa094e57540c9ed
akshat <kshatsharmaaa@gmail.com>
2025-03-13 23:05:44 +0530

    added comment and like count

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,37 +1,47 @@
-import { useQuery } from "@tanstack/react-query"
-import { supabase } from "../supabase-client"
+import { useQuery } from "@tanstack/react-query";
+import { supabase } from "../supabase-client";
 import { PostItem } from "./PostItem";
 
 export interface Post {
-    id: number;
-    title: string;
-    content: string;
-    created_at: string;
-    image_url: string;
-    avatar_url: string;
+  id: number;
+  title: string;
+  content: string;
+  created_at: string;
+  image_url: string;
+  avatar_url?: string;
+  like_count?: number;
+  comment_count?: number;
 }
 
 const fetchPosts = async (): Promise<Post[]> => {
-    const {data, error} = await supabase.from("posts").select("*").order("created_at", {ascending: false});
-    if(error) throw new Error(error.message)
-    return data as Post[];
-}
+  const { data, error } = await supabase.rpc("get_posts_with_counts");
+
+  if (error) throw new Error(error.message);
+
+  return data as Post[];
+};
 
 export const PostList = () => {
-    const {data, error, isLoading} = useQuery<Post[], Error>({queryKey: ["posts"], queryFn: fetchPosts});
+  const { data, error, isLoading } = useQuery<Post[], Error>({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+  });
 
-    if(isLoading) { return <div className="">Loading posts... </div>}
-    if(error) {
-        return <div className="">Error: {error.message}</div>
-    }
+  if (isLoading) {
+    return <div> Loading posts...</div>;
+  }
 
-    console.log(data);
+  if (error) {
+    return <div> Error: {error.message}</div>;
+  }
 
-    return (
+  console.log(data);
+
+  return (
     <div className="flex flex-wrap gap-6 justify-center">
-        {data?.map((post, key) => (
-          <PostItem post={post} key={key} />
-        ))}
+      {data?.map((post, key) => (
+        <PostItem post={post} key={key} />
+      ))}
     </div>
-    )
-}
+  );
+};
